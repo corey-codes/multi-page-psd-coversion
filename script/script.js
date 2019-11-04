a3006a098083c0ef9940167c4272589ca6bf5ffb
hellcat04 <coreyderouin@outlook.com>
2019-11-04 17:07:25 -0500

    jquery added :bomb:

---
+++ script/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.hamburger').on('click', function () {
+        // toggle class show on the menu
+        $('.menu').toggleClass('show');
+    });
+
+    $('.exit-responsive').on('click', function () {
+        // toggle class show on the menu
+        $('.menu').toggleClass('show');
+    });
+    // console.log("TESTING!");
+});
